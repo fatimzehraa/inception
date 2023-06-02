2109865825ff5008d5e53c2710034a92a43f6f46
fatimzehraa <elbouaazzaouitema@gmail.com>
2023-06-02 11:22:49 +0000

    nothing works

--- wp/script.sh
+++ wp/script.sh
@@ -2,14 +2,16 @@
 
 apt-get install -y wget
 
+apt-get -y install php7.3 php-fpm php-mysqli mariadb-client sendmail
+
 wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-
 chmod +x wp-cli.phar
-
 mv wp-cli.phar /bin/wp
 
-mkdir -p /var/www/html
+service mysql start
+service php7.3-fpm start
 
+mkdir -p /var/www/html
 
 wp core download --path="/var/www/html" --allow-root
 
@@ -24,8 +26,6 @@ wp core install --url="$WP_URL" --title="$WP_TITLE" --admin_user="$WP_ADMIN_USER
 
 pwd
 ls /var/www/html
-
-service php7.3-fpm start
 
 tail -f /dev/null
 #php-fpm7.3 -F
