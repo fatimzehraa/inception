b38c8d25974788807b199208b39477aa5508b03f
fatimzehraa <elbouaazzaouitema@gmail.com>
2023-06-08 12:44:11 +0000

    fix: wordpress is now working + structure

---
+++ Makefile
@@ -0,0 +1,24 @@
+DOCKER_COMPOSE = srcs/docker-compose.yaml
+
+all:
+	docker-compose -f $(DOCKER_COMPOSE) up -d --build
+
+up:
+	docker-compose -f $(DOCKER_COMPOSE) up -d
+
+down:
+	docker-compose -f $(DOCKER_COMPOSE) down
+
+clean:
+	docker-compose -f $(DOCKER_COMPOSE) down -v
+
+fclean:
+	docker-compose -f $(DOCKER_COMPOSE) down --volumes
+	docker container prune -f
+	docker volume prune -f
+	docker image prune -f
+	docker system prune -f
+	sudo rm -rf /home/$(USER)/data/*/* 
+	docker network prune -f
+
+re: fclean all
---
+++ srcs/requirements/wp/tools/script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+mv wp-cli.phar /bin/wp
+
+wp core download --path="/var/www/html" --allow-root
+
+wp config create --skip-check --dbname="$DB_NAME" --dbuser="$DB_USER" --dbpass="$DB_PASSWORD" --dbhost="$DB_HOST" --dbprefix="$WP_PREFIX" --path="/var/www/html" --allow-root 
+
+wp core install --url="$WP_URL" --title="$WP_TITLE" --admin_user="$WP_ADMIN_USER" --admin_password="$WP_ADMIN_PASSWORD" --admin_email="$WP_ADMIN_EMAIL" --path="/var/www/html" --allow-root 
+
+sed -i -e 's/listen =.*/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
+
+chmod -R 777 /var/www/html
+
+service php7.3-fpm start
+
+service php7.3-fpm stop
+
+php-fpm7.3 -F
